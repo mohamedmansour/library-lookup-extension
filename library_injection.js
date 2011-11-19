72f3b24859ce97792ad42a6278c4244c20cd841e
Mohamed Mansour <hello@mohamedmansour.com>
2011-11-19 14:22:51 -0500

    Amend the UI to the injection

--- library_injection.js
+++ library_injection.js
@@ -15,11 +15,26 @@ var render = function() {
   var librariesContent = document.createElement('div');
   librariesContent.innerText = 'A determiner';
   librariesContent.className = 'library_lookup_body';
+  librariesContent.innerHTML = '<ul id="result_list"></ul>';
 
-  resumeDOM.parentNode.insertBefore(librariesWrapperDOM, resumeDOM);
-  resumeDOM.parentNode.insertBefore(librariesContent, resumeDOM);
+  var parentResume = resumeDOM.parentNode;
+  parentResume.insertBefore(librariesWrapperDOM, resumeDOM);
+  parentResume.insertBefore(librariesContent, resumeDOM);
+  parentResume.insertBefore(spacerDOM.cloneNode(true), resumeDOM);
 
   return librariesContent;
+}
+
+var renderLibraries = function(data) {
+  var resultList = document.querySelector('#result_list');
+  data.forEach(function(element, index) {
+    var item = document.createElement('li');
+    var status = document.createElement('span');
+    status.innerText = element.status;
+    item.appendChild(status);
+    item.appendChild(document.createTextNode(element.district));
+    resultList.appendChild(item);
+  });
 }
 
 var isbnSpan = document.querySelector('span[id$="ISBNInfo"]');
@@ -29,7 +44,7 @@ if( isbnSpan ) {
         var ui = render();
         var isbn = isbnMatch[1];
         chrome.extension.sendRequest({method: 'GetLibraryData', data: isbn}, function(res) {
-          ui.innerText = JSON.stringify(res);
+          renderLibraries(res);
         });
     }
 }
