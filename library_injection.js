b97379557efe40ae995facfd5c19481416b4452a
Mohamed Mansour <hello@mohamedmansour.com>
2011-11-19 13:52:01 -0500

    Render contents if ISBN exsits

--- library_injection.js
+++ library_injection.js
@@ -1,28 +1,33 @@
 
-var spacerDOM = document.querySelector('.tbl_spacer');
+var render = function() {
+  var spacerDOM = document.querySelector('.tbl_spacer');
 
-var resumeDOM = document.querySelector('.lblResume_adv2');
-resumeDOM = resumeDOM.parentNode;
+  var resumeDOM = document.querySelector('.lblResume_adv2');
+  resumeDOM = resumeDOM.parentNode;
 
-var librariesDOM = document.createElement('span');
-librariesDOM.innerText = chrome.i18n.getMessage('libraryHeader');
-librariesDOM.className = 'lblLibraries_adv2 lblMore';
-var librariesWrapperDOM = document.createElement('div');
-librariesWrapperDOM.className = 'lblMoreContainer';
-librariesWrapperDOM.appendChild(librariesDOM);
+  var librariesDOM = document.createElement('span');
+  librariesDOM.innerText = chrome.i18n.getMessage('libraryHeader');
+  librariesDOM.className = 'lblLibraries_adv2 lblMore';
+  var librariesWrapperDOM = document.createElement('div');
+  librariesWrapperDOM.className = 'lblMoreContainer';
+  librariesWrapperDOM.appendChild(librariesDOM);
 
-var librariesContent = document.createElement('div');
-librariesContent.innerText = 'A determiner';
-librariesContent.className = 'library_lookup_body';
+  var librariesContent = document.createElement('div');
+  librariesContent.innerText = 'A determiner';
+  librariesContent.className = 'library_lookup_body';
 
-resumeDOM.parentNode.insertBefore(librariesWrapperDOM, resumeDOM);
-resumeDOM.parentNode.insertBefore(librariesContent, resumeDOM);
+  resumeDOM.parentNode.insertBefore(librariesWrapperDOM, resumeDOM);
+  resumeDOM.parentNode.insertBefore(librariesContent, resumeDOM);
+
+  return librariesContent;
+}
 
 var isbnSpan = document.querySelector('span[id$="ISBNInfo"]');
 if( isbnSpan ) {
     isbn = isbnSpan.innerText.match(/\((\d+)\)/);
     if( isbn ) {
-        librariesContent.innerText = isbn[1];
+        var ui = render();
+        ui.innerText = isbn[1];
     }
 }
 
