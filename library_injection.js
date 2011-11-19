e398541d024904095fe15d0f8a06ee3c7c84f8d4
Mohamed Mansour <hello@mohamedmansour.com>
2011-11-19 12:33:11 -0500

    Add new UI for libraries

--- library_injection.js
+++ library_injection.js
@@ -1,10 +1,21 @@
-alert('Hello World');
+
+function insertAfter(newNode, existingNode) {
+  if (existingNode.nextSibling) {
+    existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
+  } else {
+    existingNode.parentNode.appendChild(newNode);
+  }
+}
 
 var spacerDOM = document.querySelector('.tbl_spacer');
 
 var resumeDOM = document.querySelector('.lblResume_adv2');
 resumeDOM = resumeDOM.parentNode;
 
-var librariesDOM = resumeDOM.clone(true);
+var librariesDOM = document.createElement('span');
+librariesDOM.innerText = 'foobar';
+var librariesWrapperDOM = document.createElement('div');
+librariesWrapperDOM.appendChild(librariesDOM);
+resumeDOM.parentNode.insertBefore(librariesWrapperDOM, resumeDOM);
 
 
